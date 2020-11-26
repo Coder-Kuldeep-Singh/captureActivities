f82658f374deaffd156f69a2093b2ad6e0d139a8
mavensingh <kuldeep.avsar@gmail.com>
2020-11-26 19:00:16 +0530

    added new columns"

---
+++ db/2020_11_26_alter_columns.sql
@@ -0,0 +1,3 @@
+ALTER TABLE clicks ADD captured_time varchar(30) NOT NULL;
+ALTER TABLE clicks ADD captured_year_month varchar(30) NOT NULL;
+ALTER TABLE clicks ADD currentdate varchar(20) NOT NULL;
